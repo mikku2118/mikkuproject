bccf9e1274315d58503c49fe0278d49b999dd947
mikku2118 <138211965+mikku2118@users.noreply.github.com>
2023-06-30 16:07:09 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        ul a {
+            text-decoration: none;
+        }
+        a:hover{
+            
+            color: brown;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <center><br><br><br>
+    <div style="background-color: aquamarine; width:200px">
+    <ul>
+        <li> <h1><a href="adminhome.php">Agent login</a></h1></li><br>
+        <li><h1> <a href="userhome.php">User login</a></h1></li>
+    </ul>
+
+    </div>
+
+
+    </center>
+</body>
+</html>
